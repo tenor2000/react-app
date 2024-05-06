cfc66d5e4eaec9c3d53b3ee87c193d87eebb76ac
tenor2000 <tenor2000@gmail.com>
2024-05-06 09:47:28 -0400

    added some react examples

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import PropsExample from './PropsExample.jsx'
+import { UseTest, TodoList, Avatar, Animals } from './Test.jsx'
+import App from './state.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
